59bbbad0be9a9fd347b1ba47f024a40a47fcf6d1
Zino <zino@poulet.org>
2020-08-02 12:40:01 +0200

    + Updated package dependencies + Updated to Swift 5.2 + Updated the Sample code - Added the appKey as an optional for appKey/userID combinations

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,16 +13,16 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-       .package(url: "https://github.com/IBM-Swift/Kitura-Session.git", from: "3.2.0"),
-       .package(url: "https://github.com/IBM-Swift/Kitura-Credentials", from: "2.1.0"),
-       .package(url: "https://github.com/krugazor/DictionaryCoding", from: "0.1.0")
+       .package(url: "https://github.com/IBM-Swift/Kitura-Session.git", from: "3.3.0"),
+       .package(url: "https://github.com/IBM-Swift/Kitura-Credentials", from: "2.5.0"),
+       .package(url: "https://github.com/krugazor/DictionaryCoding", from: "0.2.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "CredentialsToken",
-            dependencies: ["KituraSession","Credentials", "DictionaryCoding"]),
+            dependencies: [.product(name: "KituraSession", package: "Kitura-Session"),.product(name: "Credentials", package: "Kitura-Credentials"), "DictionaryCoding"]),
         .testTarget(
             name: "CredentialsTokenTests",
             dependencies: ["CredentialsToken"]),
